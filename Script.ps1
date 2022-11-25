0bfcb09f4a54948eb074a6f4d3bc84a9014212fb
Ciprian Conache <cip.conache@hotmail.com>
2022-11-25 16:38:05 +0100

    Commit

--- Script.ps1
+++ Script.ps1
@@ -1,10 +1,9 @@
 #This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
-    Start-Sleep 1
-    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit
-}
+#If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+#    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+#    Start-Sleep 1
+#    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+#    Exit}
 
 #no errors throughout
 $ErrorActionPreference = 'silentlycontinue'
@@ -106,7 +105,7 @@ $apps = @(
 	
 );
 Foreach ($app in $apps){
-  Get-AppxPackage -Name $app.name| Remove-AppxPackage  
+  Get-AppxPackage -Name $app.name| Remove-AppxPackage | Write-host DisplayName
 }
 Foreach ($app in $apps){
   Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $app.name | Remove-AppxProvisionedPackage -Online
