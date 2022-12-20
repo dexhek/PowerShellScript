0b6cf0f4d28cc0400c99e412a3058c7fa0275117
Ciprian Conache <cip.conache@hotmail.com>
2022-12-20 10:47:23 +0100

    test

--- Script.ps1
+++ Script.ps1
@@ -1,4 +1,4 @@
-#This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+#This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly..
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     $Prompt = [System.Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco) 
     Switch ($Prompt) { Yes { Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
